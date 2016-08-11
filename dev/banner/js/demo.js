fd796975397de4e4e0819a94e0ac9e94130d7fe1
GuangdiZhou <947555048@qq.com>
2016-08-11 18:00:00 +0800

    moblie

--- dev/banner/js/demo.js
+++ dev/banner/js/demo.js
@@ -1,10 +1,13 @@
 function Banner(option){
 	var position = option.position,
 		option = option.option,
+		adLen = option.length,
 		arrAd,
 		arrButton,
+		previousIndex = adLen - 1,
 		currentIndex = 0,
-		previousIndex;
+		nextIndex = 1,
+		bannerWidth;
 	function createBanner(){
 		var fragment = document.createDocumentFragment();
 		arrAd = option.map(function(list,index){
@@ -13,6 +16,39 @@ function Banner(option){
 			ad.href = list.anchorHref;
 			ad.style.backgroundImage = "url(" + list.imageUrl +")";
 			fragment.appendChild(ad);
+			console.log(ad.offsetWidth);
+			var startX,
+				endX,
+				distance,
+				direction;
+			ad.addEventListener("touchstart", function(e){
+				startX = e.touches[0].clientX;
+			}, 0);
+			ad.addEventListener("touchmove", function(e){
+				//> 0 ltr 
+				//< 0 rtl
+				nextIndex = currentIndex > adLen - 1 ? 0 : currentIndex + 1;
+				distance = e.touches[0].clientX - startX;
+				direction = distance > 0;
+				this.style.left = distance + "px";
+				if(direction){
+					arrAd[previousIndex].style.left = distance - bannerWidth + "px";
+				}else{
+					arrAd[nextIndex].style.left = distance + bannerWidth + "px";
+				}
+			}, 0);
+			ad.addEventListener("touchend", function(e){
+				endX = e.changedTouches[0].clientX
+				if(Math.abs(distance) > bannerWidth / 2){
+					this.classList.add(direction ? "ltr" : "rtl");
+					}else{
+						this.classList.add("init");
+					}
+			}, 0);
+			ad.addEventListener("animationend", function(){
+				this.style.left = null;
+				this.classList.remove("init");
+			}, 0);
 			return ad;
 		});
 		position.appendChild(fragment);
@@ -52,8 +88,9 @@ function Banner(option){
 		}, 1000);
 	}
 	createBanner();
+	bannerWidth = arrAd[0].offsetWidth;
 	createIndicator();
-	autoChange();
+	// autoChange();
 }
 var banner = document.querySelector(".banner");
 ajax({
