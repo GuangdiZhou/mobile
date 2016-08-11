d090a2427bd032131198ff1662feaf8b0023352f
GuangdiZhou <947555048@qq.com>
2016-08-11 15:47:26 +0800

    moblie

--- dev/banner/js/demo.js
+++ dev/banner/js/demo.js
@@ -1,7 +1,10 @@
 function Banner(option){
 	var position = option.position,
 		option = option.option,
-		arrAd;
+		arrAd,
+		arrButton,
+		currentIndex = 0,
+		previousIndex;
 	function createBanner(){
 		var fragment = document.createDocumentFragment();
 		arrAd = option.map(function(list,index){
@@ -14,20 +17,42 @@ function Banner(option){
 		});
 		position.appendChild(fragment);
 	}
+	function createIndicator(){
+		var indicator = document.createElement("div");
+		indicator.className = "indicator";
+		arrButton = option.map(function(list,index){
+			var button = document.createElement("em");
+			button.appendChild(document.createTextNode(index + 1));
+			indicator.appendChild(button);
+			button.addEventListener("touchend",function(){
+				previousIndex = currentIndex;
+				currentIndex = index;
+				setView()
+				this.classList.add("current");
+			}, 0);
+			return button;
+		});
+		arrButton[0].classList.add("current");
+		position.appendChild(indicator);
+	}
+	function setView(){
+		arrAd[previousIndex].classList.remove("current");
+		arrAd[previousIndex].classList.add("previous");
+		arrAd[currentIndex].classList.remove("previous");
+		arrAd[currentIndex].classList.add("current");
+		arrButton[previousIndex].classList.remove("current");
+		arrButton[currentIndex].classList.remove("current");
+	}
 	function autoChange(){
-		var adLen = option.length,
-			index = 0,
-			previousIndex = option.length - 1;
+		var adLen = option.length;
 		setInterval(function(){
-			index = index < option.length - 1 ? index + 1 : 0;
-			previousIndex = index > 0 ? index - 1 : option.length - 1;
-			arrAd[previousIndex].classList.add("previous");
-			arrAd[previousIndex].classList.remove("current");
-			arrAd[index].classList.remove("previous");
-			arrAd[index].classList.add("current");
-		}, 2000);
+			currentIndex = currentIndex < adLen -1 ? currentIndex + 1 : 0;
+			previousIndex = currentIndex > 0 ? currentIndex - 1 : adLen - 1;
+			setView();
+		}, 1000);
 	}
 	createBanner();
+	createIndicator();
 	autoChange();
 }
 var banner = document.querySelector(".banner");
@@ -35,7 +60,7 @@ ajax({
 	url : "http://www.ikindness.cn/api/test/get",
 	success : function(data){
 		new Banner({
-			position :banner,
+			position : banner,
 			option : data.data
 		});
 	}
